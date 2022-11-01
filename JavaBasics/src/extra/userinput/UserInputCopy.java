907e587349815dc75d4087a5223f31ed55708b11
huda1975 <mhuda180@gmail.com>
2022-10-31 21:21:37 -0400

    Just demo purpose showing this
    Just demo purpose showing this

---
+++ JavaBasics/src/extra/userinput/UserInputCopy.java
@@ -0,0 +1,27 @@
+package extra.userinput;
+
+import java.util.Scanner;
+
+public class UserInputCopy {
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("testing github functionalities ");
+
+        System.out.println("Demo for second commit ");
+
+        System.out.println("enter your name: ");
+
+        String name = userInput.nextLine();
+
+        System.out.println("classobjectmethod.Hello "+name+", welcome to java class!!!");
+
+        System.out.println("enter your number: ");
+
+        int number = userInput.nextInt();
+
+        System.out.println("classobjectmethod.Hello number "+number+", welcome to java class!!!");
+
+    }
+}
